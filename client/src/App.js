0f0a40f79c6d03885c77c2878fefa275e0a163e1
the-rescue-ranger <indorestnorbertschool@gmail.com>
2024-10-25 12:37:00 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import Status from "./components/Status";
-import About from "./components/About";
+import Home from "./Home";
+import Status from "./Status";
+import About from "./About";
 
 function App() {
   return (
