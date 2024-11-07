1a343ee72d7814348750d6ae96d9c2ab602f759a
the-rescue-ranger <indorestnorbertschool@gmail.com>
2024-11-07 13:51:57 +0530

    Update Home.js

--- client/src/Home.js
+++ client/src/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
         Rescue Ranger
       </h1>
       <p className="text-xl mb-6 animate__animated animate__fadeIn animate__delay-1s">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
+        Live.Think.Care
       </p>
       <img src="watch-image.jpg" alt="Rescue Ranger" className="mt-4 rounded-lg shadow-lg animate__animated animate__zoomIn" />
       <a href="#features" className="mt-6 px-4 py-2 bg-white text-blue-500 rounded-lg shadow-lg transition-transform transform hover:scale-105">
