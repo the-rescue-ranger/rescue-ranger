999bf76894b4436156ec1ffa31763774a263f2b3
the-rescue-ranger <indorestnorbertschool@gmail.com>
2024-11-08 11:15:05 +0530

    Update Home.js

--- client/src/Home.js
+++ client/src/Home.js
@@ -7,8 +7,7 @@ const Home = () => {
         Rescue Ranger
       </h1>
       <p className="text-xl mb-6 animate__animated animate__fadeIn animate__delay-1s">
-        Live.Think.Care<br>
-        Introducing our innovative Smart Safety and Health Band, designed to empower women, support elderly individuals, and enhance the well-being of corporate workers. 
+        Live.Think.Care
       </p>
       <img src="watch-image.jpg" alt="Rescue Ranger" className="mt-4 rounded-lg shadow-lg animate__animated animate__zoomIn" />
       <a href="#features" className="mt-6 px-4 py-2 bg-white text-blue-500 rounded-lg shadow-lg transition-transform transform hover:scale-105">
