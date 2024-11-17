071181c9296d36d511f027415170934a73829edc
the-rescue-ranger <indorestnorbertschool@gmail.com>
2024-11-17 15:59:15 +0530

    Update Status.js

--- client/src/Status.js
+++ client/src/Status.js
@@ -46,7 +46,7 @@ const HealthChart = ({ data }) => {
 const GoogleMapWidget = ({ latitude, longitude }) => {
   if (!latitude || !longitude) return null;
 
-  const mapUrl = `https://www.google.com/maps/@${latitude},${longitude},15z?entry=ttu`;
+const mapUrl = `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.8354345094326!2d${longitude}!3d${latitude}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2z!5e0!3m2!1sen!2sau!4v1632823956034!5m2!1sen!2sau`;
 
   return (
     <Card className="transition-transform hover:scale-[1.02]">
