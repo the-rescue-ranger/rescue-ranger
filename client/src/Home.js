accf63320872fea7a50c88b830717c9fb4f02b53
the-rescue-ranger <indorestnorbertschool@gmail.com>
2024-11-23 07:08:37 +0530

    Update Home.js

--- client/src/Home.js
+++ client/src/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
       <p className="text-xl mb-6 animate__animated animate__fadeIn animate__delay-1s">
         Live.Think.Care
       </p>
-      <img src="watch-image.jpg" alt="Rescue Ranger" className="mt-4 rounded-lg shadow-lg animate__animated animate__zoomIn" />
+      <img src="logo.jpeg" alt="Rescue Ranger" className="mt-4 rounded-lg shadow-lg animate__animated animate__zoomIn" />
       <a href="https://resku.odoo.com" className="mt-6 px-4 py-2 bg-white text-blue-500 rounded-lg shadow-lg transition-transform transform hover:scale-105">
         Explore Features
       </a>
